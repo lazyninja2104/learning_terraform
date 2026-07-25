16194c014e00753a27e3b031d6bfd5c34a3be97d
lazyninja2104 <grimmhuntress@gmail.com>
2026-07-25 14:35:09 +0100

    Update to main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,24 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-
+  owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["al2023-ami-*-x86_64"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["979382823631"] # Bitnami
 }
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf install -y java-17-amazon-corretto tomcat9
+              systemctl enable tomcat9
+              systemctl start tomcat9
+              EOF
   tags = {
     Name = "HelloWorld"
   }
